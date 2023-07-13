701c7084b7b63ce36e4a95ae9028e67a4d79c267
Hadi Malik <hadia7549@gmail.com>
2023-07-13 12:58:01 -0400

    Update textToList.py

--- textToList.py
+++ textToList.py
@@ -11,7 +11,6 @@ def load_quran_text_file(file_path):
             surah_number = parts[0]
             verse_text = "|".join(parts[1:]).strip().replace("|", "")
             verses.append((surah_number, verse_text))
-            current_surah = surah_number
 
     return verses
 
@@ -24,9 +23,11 @@ def create_nested_list(verses):
     for verse in verses:
         surah_number, verse_text = verse
 
+        if current_surah is None:
+            current_surah = surah_number
+
         if surah_number != current_surah:
-            if surah_verses:
-                nested_list.append(surah_verses)
+            nested_list.append(surah_verses)
             surah_verses = []
             current_surah = surah_number
 
@@ -37,13 +38,11 @@ def create_nested_list(verses):
 
     return nested_list
 
-
 def main():
     quran_text_file = "fullQuran.txt"
     quran_verses = load_quran_text_file(quran_text_file)
     nested_list = create_nested_list(quran_verses)
 
-    # Print the nested list
     for surah in nested_list:
         print(surah)
 
